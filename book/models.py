440e0c506284825e7b901d9e3ba2afe50f48efa9
zhixiang-pan <zhixiang_pan@trendmicro.com>
2021-09-07 23:45:24 +0800

    添加手工写API的案例

---
+++ book/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.db.models.fields import NullBooleanField
+
+
+class BookInfo(models.Model):
+    title = models.CharField(max_length=20, verbose_name='书名', help_text='书名')
+    pub_date = models.DateField(verbose_name='发布日期')
+    read = models.IntegerField(default=0, verbose_name='阅读量')
+    comment = models.IntegerField(default=0, verbose_name='评论量')
+    is_delete = models.BooleanField(default=False, verbose_name='逻辑删除标记')
+    image = models.ImageField(verbose_name='图书', upload_to='book', null=True, blank=True)
+    
+    class Meta:
+        db_table = 'books'
+        verbose_name = '图书'
+        verbose_name_plural = verbose_name
+    
+    def __str__(self) -> str:
+        return self.title
+
+
+class HeroInfo(models.Model):
+    GENDER_CHOICES = (
+        (0, 'female'),
+        (1, 'male')
+    )
+
+    name = models.CharField(max_length=20, verbose_name='姓名')
+    gender = models.SmallIntegerField(choices=GENDER_CHOICES, default=0, verbose_name='性别')
+    comment = models.CharField(max_length=200, verbose_name='描述信息')
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE, verbose_name='所属图书')
+    is_delete = models.BooleanField(default=False, verbose_name='逻辑删除标记')
+
+    class Meta:
+        db_table = 'heros'
+        verbose_name = '英雄'
+        verbose_name_plural = verbose_name
+    
+    def __str__(self) -> str:
+        return self.name
---
+++ test/mannul_api.py
@@ -0,0 +1,53 @@
+"""
+用于测试mannual-api分支上手动编写的api功能
+为了测试，将Django配置文件的middleware中的
+django.middleware.csrf.CsrfViewMiddleware
+注释掉了，避免跨域问题
+"""
+import requests
+import json
+
+
+def test_post():
+    '''验证手动编写的post api'''
+    url = 'http://127.0.0.1:8000/books/'
+    auth = ('admin', 'admin')
+    data = {
+        'title': '斗罗大陆',
+        'pub_date': '2015-12-12',
+        'comment': '200',
+        'read': '100',
+        'image': ''
+    }
+    data = json.dumps(data)
+    result = requests.post(url, data, auth=auth)
+    if result.status_code == 201:
+        print('资源创建成功，POST API测试成功')
+        print(result.content)
+    else:
+        print('资源创建失败，POST API测试失败')
+
+
+def test_delete():
+    '''
+    验证手动编写的delete api;
+    View里面写的是逻辑删除，所以数据不会实际删除，只是is_delete标记变为1
+    '''
+    url = 'http://127.0.0.1:8000/books/6'
+    auth = ('admin', 'admin')
+    result = requests.delete(url, auth=auth)
+    if result.status_code == 204:
+        print('资源删除成功，DELETE API测试成功')
+    else:
+        print('资源删除失败，DELETE API测试失败')
+    
+
+def main():
+    # test_post()
+    # test_delete()
+    pass
+
+
+if __name__ == '__main__':
+    main()
+    
